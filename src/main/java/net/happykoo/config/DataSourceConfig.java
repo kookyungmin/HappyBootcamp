8f8a95b381b860bce51c2525fafa5bd47438b066
kookyungmin <rudals4549@naver.com>
2024-09-22 01:04:11 +0900

    feat: connection pool(hikari) 적용

--- src/main/java/net/happykoo/config/DataSourceConfig.java
+++ src/main/java/net/happykoo/config/DataSourceConfig.java
@@ -1,11 +1,14 @@
 package net.happykoo.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -18,26 +21,12 @@ import java.sql.Driver;
 
 @Configuration
 public class DataSourceConfig {
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Value("${spring.datasource.jdbc-url}")
-    private String jdbcUrl;
-
-    @Value("${spring.datasource.username}")
-    private String userName;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
     @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public DataSource dataSource() throws ClassNotFoundException {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        dataSource.setDriverClass((Class<Driver>) Class.forName(driverClassName));
-        dataSource.setUrl(jdbcUrl);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(password);
+        HikariDataSource dataSource = DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
 
         return dataSource;
     }
